bce6e4b86338d8a68a10035a66fa825949a504e4
Orianna_S <shibiyun2013@163.com>
2021-05-21 12:25:34 +0800

    fix wanplus

--- api/wanplus.py
+++ api/wanplus.py
@@ -6,9 +6,8 @@ from selenium.webdriver.support.ui import Select
 import time
 
 """
-TODO:
-1. selenium scraping for play stats for each player
-2. api integration
+This api mainly focus on fetching the performance measurement statistics on msi play.
+It wont be intergrated to frontend api currently as the server response from wanplus is very slow.
 """
 class WanPlusMsi():
     def __init__(self):
@@ -28,24 +27,62 @@ class WanPlusMsi():
         # table 2 for warding and resource control is arranged as:
         # | order | teamname | dragonkillsPergame | dragonkillspercentage | baronkillsPergame | baronkillspercentage | wardsplacedpermin | wardskilledpermin | wardskilledrate | towertakensPergame | towerdeathsPergame |
         table1 = self.tableToArray()
-        tab2 = self.driver.find_element_by_class_name('detail-list-title').find_elements_by_css_selector('div')[1]
-        tab2.click()
+        tab = self.driver.find_element_by_class_name('detail-list-title').find_elements_by_css_selector('div')[1]
+        tab.click()
         table2 = self.tableToArray()
         return [table1, table2]
 
+    def getAllPlayerStats(self):
+        # table 3 for k/d/a statistics is arranged as:
+        # | order | playername | teamname | role | appearedTimes | kda | attendrate | killsPergame | mostkills | deathsPergame | mostdeaths | assistsPergame |
+        # table 4 for farming and team contribution is arranged as:
+        # | order | playername | mostassists | goldsPermin | lasthitPermin | damagetoheroPermin | damagetoheroPercent | damagetakenPermin | damagetakenPercent | wardsplacedPermin | wardskilledPermin |
+        link = self.driver.find_element_by_xpath('//*[@href="/lol/playerstats"]')
+        link.click()
+        time.sleep(1)
+        table3 = self.getPaginatedTable()
+        tab2 = self.driver.find_element_by_class_name('detail-list-title').find_elements_by_css_selector('div')[1]
+        tab2.click()
+        table4 = self.getPaginatedTable()
+        return [table3, table4]
+
+    def getPaginatedTable(self):
+        start = 1
+        end = len(self.driver.find_elements_by_class_name('paginate_button ')) - 1
+        table = []
+        while (start < end):
+            btn = self.driver.find_elements_by_class_name('paginate_button ')[start]
+            btn.click()
+            time.sleep(0.5)
+            tb = self.tableToArray()
+            table += tb
+            start += 1
+        return table
+
     def tableToArray(self):
-        teamStats = []
+        stats = []
         dataTable = self.driver.find_element_by_id('DataTables_Table_0')
         lines = dataTable.find_elements_by_css_selector('tr')
         for line in lines:
             data = line.find_elements_by_css_selector('td')
             if len(data) != 0:
-                team = []
+                dt = []
                 for item in data:
                     if item.text != '':
-                        team.append(item.text)
-                teamStats.append(team)
-        return teamStats
+                        dt.append(self.chineseTransformer(item.text))
+                stats.append(dt)
+        return stats
+
+    def chineseTransformer(self, str):
+        if str == '打野':
+            return "Jungle"
+        elif str == '上单':
+            return "Top"
+        elif str == '中单':
+            return "Mid"
+        elif str == '辅助':
+            return "Support"
+        else:
+            return str
 
 wanplusmsi = WanPlusMsi()
-# print(wanplusmsi.getAllTeamStats())
